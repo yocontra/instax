12b2f83ffa394a6e1d2e668c09e577cba84197ab
Contra <contra@maricopa.edu>
2015-07-06 23:14:45 -0700

    initial

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should');
+var instax = require('../');
+
+describe('FUNCTIONNAME()', function() {
+  it('should TASKNAME', function(done) {
+    should.exist(instax);
+    done();
+  });
+});
